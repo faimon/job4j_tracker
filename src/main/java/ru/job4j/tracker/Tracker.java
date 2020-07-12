d275f16ce97d542e80d80e88a5e225029248a20a
andreykatanov <faimon96@gmail.com>
2020-07-12 21:10:57 +0700

    6. Метод замены заявки. Tracker.replace [#317065]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -29,12 +29,23 @@ public class Tracker {
         return rsl;
     }
 
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        items[index] = item;
+        items[index].setId(id);
+        return index != -1;
+    }
+
     public Item findById(int id) {
-        Item rsl = null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
