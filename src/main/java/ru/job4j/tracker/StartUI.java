71165195e36e9aa2752d820c99802e699b865196
andreykatanov <faimon96@gmail.com>
2020-07-12 19:59:13 +0700

    5. Tracker - хранилище [#317069]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item());
+        System.out.println(tracker.findById(1));
+    }
+}
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,22 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = null;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item.getName().equals(key)) {
+                rsl[count++] = item;
+            }
+        }
+        return rsl;
     }
 
     public Item findById(int id) {
