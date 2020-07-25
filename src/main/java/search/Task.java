b579af5c4fe46151e304c3e5ea8f339e43a02b8a
andreykatanov <faimon96@gmail.com>
2020-07-25 20:21:44 +0700

    1. Уникальные задачи. [#316981]

--- src/main/java/search/Task.java
+++ src/main/java/search/Task.java
@@ -3,10 +3,21 @@ package search;
 public class Task {
     private String desc;
     private int priority;
+    private String number;
+    private String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
 
     public Task(String desc, int priority) {
         this.desc = desc;
         this.priority = priority;
+    }
+
+    public String getNumber() {
+        return number;
     }
 
     public String getDesc() {
