b4a75163a6bd01ffc2205329a8f854b52234dfa4
andreykatanov <faimon96@gmail.com>
2020-07-25 13:30:56 +0700

    2. Очередь с приоритетом на LinkedList [#316969]

--- src/main/java/search/PriorityQueue.java
+++ src/main/java/search/PriorityQueue.java
@@ -14,9 +14,12 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() < task.getPriority()) {
-                index = element.getPriority();
+            if (tasks.size() == 0) {
+                break;
             }
+            if (element.getPriority() < task.getPriority()) {
+                    index++;
+                }
         }
         this.tasks.add(index, task);
     }
