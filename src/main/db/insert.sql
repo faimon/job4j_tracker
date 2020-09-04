e1d17094e96b0c7ef077baf88084e8ebe2c006d8
andreykatanov <faimon96@gmail.com>
2020-09-04 11:30:21 +0700

    1. Sql схема для проекта Tracker. [#337977]

---
+++ src/main/db/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO items(name) VALUES ('test stroka 1');
+INSERT INTO items(name) VALUES ('test stroka 2');
+INSERT INTO items(name) VALUES ('test stroka 3');
+INSERT INTO items(name) VALUES ('test stroka 4');
+INSERT INTO items(name) VALUES ('test stroka 5');
+INSERT INTO items(name) VALUES ('test stroka 6');
+INSERT INTO items(name) VALUES ('test stroka 7');
+INSERT INTO items(name) VALUES ('test stroka 8');
